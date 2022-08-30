3f5091aa4b28da1c494afc932af0473b5a777539
1999KhushiSrivastava <rimghim220@gmail.com>
2022-08-30 23:27:41 +0530

    tryandcatch

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,36 +1,46 @@
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
 //Authentication
-const authentication =  function(req,res,next){
-const token = req.headers["x-Auth-token"]||req.headers["x-auth-token"];  //case sensitive
-//   if (!token) token = req.headers["x-auth-token"];
+const authentication = function (req, res, next) {
+  try {
+    const token = req.headers["x-Auth-token"] || req.headers["x-auth-token"];  //case sensitive
+    //   if (!token) token = req.headers["x-auth-token"];
 
-  //If no token is present in the request header return error. This means the user is not logged in.
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-  try{
-    let decoded= jwt.verify(token,'functionup-plutonium-very-very-secret-key')// authentication
-    req.token= decoded
-  }catch(error){
-    // console.log(error)
-    return res.send({msg:error.message})
+    //If no token is present in the request header return error. This means the user is not logged in.
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+
+    let decoded = jwt.verify(token, 'functionup-plutonium-very-very-secret-key', function (error, decoded) {
+      if (error) {
+        return res.status(401).send({ msg: error.message })      //unauthorised
+      }
+      req.token = decoded
+      next()
+    })// authentication
   }
-  
-  next()
+  catch (error) {
+    return res.status(500).send({ msg: error.message })         //server side error
+  }
 }
-const authorisation = async function(req,res,next){
-let userId = req.params.userId;
-let user = await userModel.findById(userId)
-if(!user){
- return res.send({msg:"user does not exists"})
+const authorisation = async function (req, res, next) {
+
+  try {
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId)
+    if (!user) {
+      return res.status(404).send({ msg: "user not found" })
+    }
+    let userloggedIn = req.token.userId //token id
+    if (userId != userloggedIn) {
+      return res.status(403).send({ msg: "authorisation failed" })
+    }
+    else {
+      req.userId = user._id
+      next()
+    }
+  }
+  catch (error) {
+    return res.status(500).send({ msg: error.message })
+  }
 }
-let userloggedIn = req.token.userId //token id
-if(userId != userloggedIn){
-  return res.send({msg:"authorisation failed"})
-}
-else{
-  req.userId=user._id
-  next()
-}
-}
-  module.exports.authentication=authentication
-  module.exports.authorisation=authorisation
+module.exports.authentication = authentication
+module.exports.authorisation = authorisation
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,23 +7,43 @@ const userModel = require("../models/userModel");
 const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
-  //the second parameter is always the response
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  console.log(req.newAtribute);
-  res.send({ msg: savedData });
+  //the second parameter is always the 
+  try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    console.log(req.newAtribute);
+    res.status(201).send({ msg: savedData }); // create
+  }
+  catch (error) {
+    res.status(500).send({ msg: error.message })
+  }
 };
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
 
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not correct",
-    });
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.status(400).send({                      // bad request
+        status: false,
+        msg: "username or the password is not correct",
+      });
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "plutonium",
+        organisation: "FunctionUp",
+      },
+      "functionup-plutonium-very-very-secret-key"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(201).send({ status: true, data: token });    // create 
+  }
+  catch (error) {
+    res.status(500).send({ msg: error.message })
+  }
 
   // Once the login is successful, create the jwt token with sign function
   // Sign function has 2 inputs:
@@ -31,20 +51,11 @@ const loginUser = async function (req, res) {
   // The decision about what data to put in token depends on the business requirement
   // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
   // The same secret will be used to decode tokens 
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "plutonium",
-      organisation: "FunctionUp",
-    },
-    "functionup-plutonium-very-very-secret-key"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
+
 };
 
 const getUserData = async function (req, res) {
-  
+
   // If a token is present then decode the token with verify function
   // verify takes two inputs:
   // Input 1 is the token to be decoded
@@ -58,8 +69,15 @@ const getUserData = async function (req, res) {
   // if (!decodedToken)
   //   return res.send({ status: false, msg: "token is invalid" });
 
-  let userDetails= await userModel.findById(req.userId)
-  return res.send({ status: true, data: userDetails });
+  try {
+    let userDetails = await userModel.findById(req.userId)
+    return res.status(200).send({ status: true, data: userDetails }); //successful response
+  }
+  catch (error) {
+    return res.status(500).send({
+      msg: error.message
+    })
+  }
   // Note: Try to see what happens if we change the secret while decoding the token
 };
 
@@ -69,27 +87,34 @@ const updateUser = async function (req, res) {
   // Check if the token present is a valid token
   // Return a different error message in both these cases
 
-  
 
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: req.userId}, userData,{new:true});
-  return res.send({ status: updatedUser, data: updatedUser });
+
+  try {
+    let userData = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({ _id: req.userId }, userData, { new: true });
+    return res.status(200).send({ status: updatedUser, data: updatedUser }); // successfull response
+  }
+  catch (error) {
+    return res.status(500).send({ msg: error.message })
+  }
 };
 
-let deleteUser= async function(req, res){
+let deleteUser = async function (req, res) {
 
-  
-  let deletedUser = await userModel.findOneAndUpdate(
-    {_id: req.userId}, //condition
-    {$set:{isDeleted:true}},{new:true})//update,);
-    
-     
-    return res.send({status :deletedUser, data: deletedUser })
 
+  try {
+    let deletedUser = await userModel.findOneAndUpdate(
+      { _id: req.userId }, //condition
+      { $set: { isDeleted: true } }, { new: true })//update,);
+    return res.status(200).send({ status: deletedUser, data: deletedUser })
   }
+  catch {
+    return res.status(500).send({ msg: error.message })
+  }
+}
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
-module.exports.deleteUser=deleteUser;
+module.exports.deleteUser = deleteUser;
